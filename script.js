effc81794a8e307a46e238e792f70a84264908de
Rizki033 <rizkiabdelhadi4@gmail.com>
2026-04-05 21:14:41 +0100

    feat: add HTTPS check

---
+++ script.js
@@ -0,0 +1,17 @@
+function analyzeURL() {
+    const url = document.getElementById("urlInput").value;
+    let result = "";
+
+    if (!url) {
+        result = "Please enter a URL";
+    }
+
+    document.getElementById("result").innerText = result;
+}
+
+if (!url.startsWith("https://")) {
+    result += "Not secure (no HTTPS)\n";
+} else {
+    result += "Uses HTTPS\n";
+}
+
